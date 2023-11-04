21dd995ca7a5bb16c7a43cec06b85485127bf814
Kevin Plattret <kevin@plattret.com>
2023-11-04 16:09:03 +0000

    Git now set to not ignore case

---
+++ components/Layout.tsx
@@ -0,0 +1,45 @@
+import Menu from 'components/Menu'
+
+import Head from 'next/head'
+import Link from 'next/link'
+import { Analytics } from '@vercel/analytics/react'
+
+export default function Layout({
+  children,
+  title
+}: {
+  children: React.ReactNode
+  title?: string
+}) {
+  const github = <Link href="https://github.com/kplattret/kevinplattret.com">GitHub</Link>
+  const email = <Link href="mailto:kevin@plattret.com">email</Link>
+  const pgpKey = <Link href="https://keys.openpgp.org/search?q=kevin@plattret.com">PGP key</Link>
+  const matrix = <Link href="https://matrix.to/#/@kplattret:matrix.org">Matrix</Link>
+
+  return (
+    <>
+      <Head>
+        <title>{`${title ? title + " | " : ""} Kevin Plattret, Software Engineer`}</title>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/icons/favicon.png" />
+      </Head>
+
+      <header className="header">
+        <Link href="/" className="alt">Kevin Plattret</Link>
+        <Menu items={["reads", "thoughts", "work"]} />
+      </header>
+
+      <main className="content">
+        {children}
+      </main>
+
+      <footer className="footer">
+        <p>Written by Kevin Plattret in London and other places. Source code available on {github}.
+          Feel free to reach out via {email} ({pgpKey}) or on {matrix}.</p>
+      </footer>
+
+      <Analytics />
+    </>
+  )
+}
